da18def0728850a1563503d2b74e3b2fd1ba99ff
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-10-31 21:56:14 +0000

    Added icons and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white30,
+          appBar: AppBar(
+            title: Text('I am Poor'),
+            backgroundColor: Colors.white10,
+          ),
+          body: const Center(
+              child: Image(
+            image: AssetImage('images/NoTextLogo.png'),
+          )),
+        ),
+      ),
+    );
